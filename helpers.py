2bc4e1318ad09486164b36328696f58a3d2bdf22
Sundar Sitaula <storymachinedata@gmail.com>
2023-03-22 09:42:22 +0100

    added new files

--- helpers.py
+++ helpers.py
@@ -11,16 +11,6 @@ month = datetime.today().month
 day = datetime.today().day
 
 
-#files
-
-# palantir_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/xv0G0VRws9pU6ZsMf1uY2g/palantir_1.csv'
-
-# paula_cipi_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/qmmv9cspSmqDQpn0QoRDdg/paula_cipi_1.csv'
-# jan_hie_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/DpKxT9bGGT1HtVTj8BoFnA/jan_hiesserich.csv'
-# kath_brienne_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/w2Tf62a4RFxsvwk63Tbs8A/katharina_brienne.csv'
-
-
-
 
 storymch_logo = "https://storymachine.mocoapp.com/objects/accounts/a201d12e-6005-447a-b7d4-a647e88e2a4a/logo/b562c681943219ea.png"
 
@@ -31,8 +21,35 @@ filters = { 'Total Interaction: High to Low' : ['Total Interactions', False],
             'Posts: Oldest First': ['date',True]}
 
 
+mapper = { 'https://www.linkedin.com/company/amaneos/posts/?feedView=all' : 'Amaneos' ,
+            'https://www.linkedin.com/company/asteriio/posts/?feedView=all': 'Asteri'  ,
+         'https://www.linkedin.com/company/balcke-duerr/posts/?feedView=all': 'Balcke-Dürr Group',
+        	'https://www.linkedin.com/company/cimos/posts/?feedView=all':'Cimos',
+        	'https://www.linkedin.com/company/clecim/posts/?feedView=all':'Clecim',
+        	'https://www.linkedin.com/company/donges-group/posts/?feedView=all':'Donges Group',
+        	'https://www.linkedin.com/company/fasana-gmbh/posts/?feedView=all':'FASANA',
+        	'https://www.linkedin.com/company/frigoscandia/posts/?feedView=all':'Frigoscandia',
+        	'https://www.linkedin.com/company/ganter-group/posts/?feedView=all':'Ganter Group',
+        	'https://www.linkedin.com/company/gemini-rail-group/posts/?feedView=all':'Gemini Rail Groups und ADComms',
+        	'https://www.linkedin.com/company/guascorenergy/posts/?feedView=all':'Guascor Energy',
+        	'https://www.linkedin.com/company/iinovis/posts/?feedView=all':'iinovis Group',
+        	'https://www.linkedin.com/company/keeeper-gmbh/posts/?feedView=all':'keeeper Group',
+        	'https://www.linkedin.com/company/kico-group/posts/?feedView=all':'Kico und ISH Group',
+        	'https://www.linkedin.com/company/la-rochette-cartonboard/posts/?feedView=all':'La Rochette Cartonboard',
+        	'https://www.linkedin.com/company/nem/posts/?feedView=all':'NEM Energy',
+        	'https://www.linkedin.com/company/palmia-oy/posts/?feedView=all':'Palmia',
+        	'https://www.linkedin.com/company/plati-spa/posts/?feedView=all':'Plati Group',
+        	'https://www.linkedin.com/company/primotecsgroup/posts/?feedView=all':'PrimoTECS Group',
+        	'https://www.linkedin.com/company/repartim/posts/?feedView=all':'Repartim Group',
+        	'https://www.linkedin.com/company/sabo-maschinenfabrik-gmbh/posts/?feedView=all':'SABO',
+        	'https://www.linkedin.com/company/special-melted-products-ltd/posts/?feedView=all':'Special Melted Products',
+        	'https://www.linkedin.com/company/valtitubes/posts/?feedView=all':'VALTI',
+            'https://www.linkedin.com/company/mutares/posts/?feedView=all': 'Mutares SE & CO.KGaA'}
 
 
+
+
+@st.cache
 def read_file(filename):
     df =pd.read_csv(filename)
     df = df.dropna(how='any', subset=['textContent'])
@@ -56,6 +73,36 @@ def read_file(filename):
 
 
 
+@st.cache
+def read_file_sp(filename):
+    df =pd.read_csv(filename)
+    df = df.dropna(how='any', subset=['postContent'])
+    # df.drop(['error', 'timestamp', 'sharedPostUrl','sharedPostProfileUrl',
+    #         'sharedJobUrl','videoUrl','sharedPostCompanyUrl'], axis=1, inplace=True)
+
+    df['postDate'] = df.postUrl.apply(getActualDate)
+    df = df.dropna(how='any', subset=['postDate'])
+    df['date'] =  pd.to_datetime(df['postDate'])
+
+    df['company_name'] =  df.profileUrl.apply(lambda x : mapper[x])
+
+    df.drop_duplicates(subset=['postUrl'], inplace=True)
+    df = df.reset_index(drop=True)
+    df['Total Interactions'] = df['likeCount'] + df['commentCount']
+    df['likeCount'] = df['likeCount'].fillna(0)
+    df['commentCount'] = df['commentCount'].fillna(0)
+    df['Total Interactions'] = df['Total Interactions'].fillna(0)
+    df['likeCount'] = df['likeCount'].astype(int)
+    df['commentCount'] = df['commentCount'].astype(int)
+    df['Total Interactions'] = df['Total Interactions'].astype(int)
+    #df['Keyword']  = df['category']
+    df['yy-dd-mm'] = pd.to_datetime(df.date).dt.strftime('%Y/%m/%d')
+    
+    return df
+
+
+
+
 
 
 def getActualDate(url):
@@ -70,8 +117,21 @@ def getActualDate(url):
 
 
 def printFunction(i, rows, dataframe):
-    if not pd.isnull(rows['profileImgUrl']):
-        st.image(rows['profileImgUrl'], width=150)
+   
+    if not pd.isnull(rows['companyUrl']):
+        st.subheader(rows.companyName)
+        st.write('Company Account')
+      
+        st.info(rows['textContent'])
+        st.write('Total Interactions 📈:  ',rows['Total Interactions'])
+        st.write('Likes 👍:  ',rows['likeCount']) 
+        st.write('Comments 💬:  ',rows['commentCount'])
+        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
+        with st.expander('Link to this Post 📮'):
+                st.write(rows['postUrl']) #linktoPost
+        with st.expander('Link to  Profile 🔗'):
+                st.write(rows['companyUrl']) #linktoProfile
+
 
     if not pd.isnull(rows['profileUrl']):
         #st.image(rows['profileImgUrl'], width=150)
@@ -79,8 +139,30 @@ def printFunction(i, rows, dataframe):
         st.write('Personal Account')
         st.write(rows['title']) #postType
         st.write('-----------')
-        if not pd.isnull(rows['postImgUrl']):
-            st.image(rows['postImgUrl'])
+       
+        st.info(rows['textContent'])  #postrowsontent
+        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
+        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
+        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
+        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
+        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
+        with st.expander('Link to this Post 📮'):
+                st.write(rows['postUrl']) #linktoPost
+        with st.expander('Link to  Profile 🔗'):
+                st.write(rows['profileUrl']) #linktoProfile
+
+
+
+
+def printFunction_search(i, rows, dataframe):
+   
+    if not pd.isnull(rows['profileUrl']):
+        #st.image(rows['profileImgUrl'], width=150)
+        st.subheader(dataframe.fullName[i])
+        st.write('Personal Account')
+        st.write(rows['title']) #postType
+        st.write('-----------')
+       
         st.info(rows['textContent'])  #postrowsontent
         st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
         st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
@@ -92,14 +174,18 @@ def printFunction(i, rows, dataframe):
         with st.expander('Link to  Profile 🔗'):
                 st.write(rows['profileUrl']) #linktoProfile
     
-    if not pd.isnull(rows['logoUrl']):
-        st.image(rows['logoUrl'], width=150)
-        st.subheader(rows['companyName'])
-        st.write('Corporate Account')
-        st.write('👥:  ',rows['followerCount'])
-        if not pd.isnull(rows['postImgUrl']):
-            st.image(rows['postImgUrl'])
-        st.info(rows['textContent'])  #postrowsontent
+
+
+
+def printFunction_posts(i, rows, dataframe):
+    if not pd.isnull(rows['profileUrl']):
+        #st.image(rows['profileImgUrl'], width=150)
+        st.subheader(dataframe.company_name[i])
+        #st.write('Personal Account')
+        st.write(rows['type']) #postType
+        st.write('-----------')
+       
+        st.info(rows['postContent'])  #postrowsontent
         st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
         st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
         st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
@@ -107,8 +193,12 @@ def printFunction(i, rows, dataframe):
         st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
         with st.expander('Link to this Post 📮'):
                 st.write(rows['postUrl']) #linktoPost
-        with st.expander('Link to  Company Profile 🔗'):
-                st.write(rows['companyUrl']) #linktoProfile
+        with st.expander('Link to  Profile 🔗'):
+                st.write(rows['profileUrl']) #linktoProfile
+    
+
+
+
 
 
 def printError():
